871c8bffa54bf4775f55a5a54fedb72ef33f27ea
ravi4rails <ravionrails@gmail.com>
2020-03-20 16:25:58 +0530

    working on webhook integration

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,10 @@
 Rails.application.routes.draw do
+  mount StripeEvent::Engine, at: '/stripe/webhook'
   resources :users
   resources :payments, only: [:create]
 
   get '/checkout' => "users#checkout"
   root "users#index"
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
